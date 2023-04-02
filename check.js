522a4f02e96e7572a95501106c3b0402d5fbfa15
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-04-02 16:32:18 +0600

    == and ===

---
+++ check.js
@@ -0,0 +1,28 @@
+const x = [4,6];
+
+const z= null;
+
+if(!z){
+    // console.log('value is falsy');
+}
+
+const y = 4;
+
+
+if(!!y)
+{
+    // console.log('value is true');
+}
+
+const first = 0;
+const second = false;
+// == equal dile dekabe je..ai 2 ta same .
+// kintu === equal dile dekabe je ..same na..=== equal bujay  de type and value 
+if(first == second)
+{
+    console.log('value are equal');
+}
+else
+{
+    console.log('value is not equal');
+}
